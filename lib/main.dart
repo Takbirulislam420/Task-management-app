08595134d4438d03f4053be86811bf845f26fe70
Takbir <Takbirulislam420@gmail.com>
2025-05-03 21:46:42 +0800

    add firebase with this app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'package:task_management_app/TaskManagementApp.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const TaskManagementApp()); //takbir
 }
